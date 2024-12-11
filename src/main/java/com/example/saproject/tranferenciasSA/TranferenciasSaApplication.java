a71735214cdb0d6197f7e877b436f0b1d3a00506
samuelsadev <samuelsa.dev@gmail.com>
2024-12-11 18:56:43 -0300

    first commit

---
+++ src/main/java/com/example/saproject/tranferenciasSA/TranferenciasSaApplication.java
@@ -0,0 +1,13 @@
+package com.example.saproject.tranferenciasSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranferenciasSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranferenciasSaApplication.class, args);
+	}
+
+}
