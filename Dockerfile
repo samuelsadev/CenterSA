55ebe49f80943bfbf47ecd936bac066320273d42
samuelsadev <samuelsa.dev@gmail.com>
2025-01-24 02:23:30 -0300

    Configurando queue

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /app
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/saproject/centerSA/config/RabbitMQConfig.java
+++ src/main/java/com/saproject/centerSA/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package com.saproject.centerSA.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,23 +15,46 @@ public class RabbitMQConfig {
     public static final String ROUTING_KEY = "transactions_routing_key";
 
     @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("rabbitmq");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.initialize();
+        return rabbitAdmin;
+    }
+
+
+    @Bean
     public Queue transactionsQueue() {
         System.out.println("Creating queue: " + QUEUE_TRANSACTIONS);
-        return new Queue(QUEUE_TRANSACTIONS, true);
+        Queue queue = new Queue(QUEUE_TRANSACTIONS, true);
+        System.out.println("Queue created: " + queue.getName());
+        return queue;
     }
 
     @Bean
     public DirectExchange transactionsExchange() {
         System.out.println("Creating exchange: " + EXCHANGE_TRANSACTIONS);
-        return new DirectExchange(EXCHANGE_TRANSACTIONS);
+        DirectExchange exchange = new DirectExchange(EXCHANGE_TRANSACTIONS);
+        System.out.println("Exchange created: " + exchange.getName());
+        return exchange;
     }
 
     @Bean
     public Binding transactionsBinding(Queue transactionsQueue, DirectExchange transactionsExchange) {
         System.out.println("Binding queue to exchange with routing key: " + ROUTING_KEY);
-        return BindingBuilder.bind(transactionsQueue)
+        Binding binding = BindingBuilder.bind(transactionsQueue)
                 .to(transactionsExchange)
                 .with(ROUTING_KEY);
+        System.out.println("Binding created: " + binding);
+        return binding;
     }
+
 
 }
